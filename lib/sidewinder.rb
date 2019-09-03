10e735e2bff7d1fbc9665bb42527414c441bc6fc
petejh <petejh@users.noreply.github.com>
2019-09-02 22:58:45 -0700

    Carve a maze using the Sidewinder algorithm

---
+++ lib/sidewinder.rb
@@ -0,0 +1,43 @@
+class Sidewinder
+  def self.carve(grid: grid)
+    grid.each_row do |row|
+      run = []
+      row.each do |cell|
+        run << cell
+
+        if close_out_run?(grid, cell)
+          member = run.sample
+
+          north = grid[member.row - 1, member.column]
+          member.link_to(north) if north
+          north.link_to(member) if north
+          run.clear
+        else
+          east = grid[cell.row, cell.column + 1]
+          cell.link_to(east)
+          east.link_to(cell)
+        end
+      end
+    end
+
+    grid
+  end
+
+  private
+
+  def self.close_out_run?(grid, cell)
+    at_east_border?(grid, cell) || (!at_north_border?(grid, cell) && rand(2) == 0)
+  end
+
+  def self.at_east_border?(grid, cell)
+    east = grid[cell.row, cell.column + 1]
+
+    east.nil?
+  end
+
+  def self.at_north_border?(grid, cell)
+    north = grid[cell.row - 1, cell.column]
+
+    north.nil?
+  end
+end
