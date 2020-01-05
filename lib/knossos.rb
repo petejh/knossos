c074e1123400a2921e70d3581ffa53f251b6e2b4
petejh <petejh.code@q.com>
2020-01-05 11:37:26 -0800

    Declare Knossos namespace at the project root

--- lib/knossos.rb
+++ lib/knossos.rb
@@ -1,8 +1,10 @@
-require 'knossos/version'
-
 require 'knossos/algorithm'
 require 'knossos/cell'
 require 'knossos/display'
 require 'knossos/error'
 require 'knossos/grid'
 require 'knossos/png_adapter'
+require 'knossos/version'
+
+module Knossos
+end
